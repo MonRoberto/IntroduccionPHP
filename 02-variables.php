df96186e82726579aa2c533f3ba8e7d96a3e80bc
MonRoberto <roberto.quiroz117@outlook.com>
2025-02-11 20:58:20 -0600

    Variables y constantes en PHP

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,20 @@
 <?php include 'includes/header.php';
 
 
+$nombre = "Mon";
+$nombre = "Mon 2";
+
+echo $nombre;
+var_dump($nombre);
+
+define('constante', "Este es el valor de la constante");
+echo constante;
+
+const constante2 = "Hola 2";
+echo constante2;
+
+$nombreCliente = "Pao";
+$nombre_cliente ="Pao";
 
 
 include 'includes/footer.php';
